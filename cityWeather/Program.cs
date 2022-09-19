618972e76904646f923e886056ccbd594bceece4
mehmetizbul <mehmetizbul@gmail.com>
2022-09-19 16:06:03 +0300

    Applied coding standards using PVS-Studio and StyleCop

--- cityWeather/Program.cs
+++ cityWeather/Program.cs
@@ -3,91 +3,95 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-//NuGet package to handle JSON response deserialize.
+// NuGet package to handle JSON response deserialize.
+
 using Newtonsoft.Json;
 
-namespace cityWeather
+namespace CityWeather
 {
-    //Class declarations to be mapped to the relevant JSON response.
+    // Class declarations to be mapped to the relevant JSON response.
     public class City
     {
-        public string name { get; set; }
-        public string latitude { get; set; }
-        public string longitude { get; set; }
+        public string Name { get; set; }
 
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
     }
+
     public class Weather
     {
-        public Location location { get; set; }
-        public Forecast forecast { get; set; }
+        public Location Location { get; set; }
+
+        public Forecast Forecast { get; set; }
     }
+
     public class Location
     {
-        public string name { get; set; }
+        public string Name { get; set; }
     }
 
     public class Forecast
     {
-        public Forecastday[] forecastday { get; set; }
+        public Forecastday[] Forecastday { get; set; }
     }
 
     public class Forecastday
     {
-        public Day day { get; set; }
+        public Day Day { get; set; }
     }
+
     public class Day
     {
-        public Condition condition { get; set; }
+        public Condition Condition { get; set; }
     }
+
     public class Condition
     {
-        public string text { get; set; }
+        public string Text { get; set; }
     }
 
     class Program
     {
-        //The configuration parameters.
-        static HttpClient client = new HttpClient();
-        static string baseTUI = "https://api.musement.com/";
-        static string baseApiWeather = "http://api.weatherapi.com/v1/";
-        static string weatherApiKey = "dd003f4d64e04f3cb6955131221409";
-        static int weatherDays = 2;
+        // The configuration parameters.
+        private static HttpClient client = new HttpClient();
+        private static string baseTUI = "https://api.musement.com/";
+        private static string baseApiWeather = "http://api.weatherapi.com/v1/";
+        private static string weatherApiKey = "dd003f4d64e04f3cb6955131221409";
+        private static int weatherDays = 2;
 
         static void Main(string[] args)
         {
-            //Following 2 lines are added to fix "Could not create SSL/TLS secure channel" error.
+            // Following 2 lines are added to fix "Could not create SSL/TLS secure channel" error.
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            //Execution of the application logic.
+            // Execution of the application logic.
             RunAsync().GetAwaiter().GetResult();                        
         }
-        //The function returns the list of cities from TUI API.
-        static async Task <List<City>> GetCityAsync()
+        // The function returns the list of cities from TUI API.
+        private static async Task <List<City>> GetCityAsync()
         {                                       
             var response = await client.GetAsync(baseTUI + "api/v3/cities/");
             var responseString = await response.Content.ReadAsStringAsync();
                 
-            //Deserializing the JSON response into "City" class using "List".
+            // Deserializing the JSON response into "City" class using "List".
             var cityList = JsonConvert.DeserializeObject<List<City>>(responseString);
 
-            return cityList;                
-            
+            return cityList;                            
         }
 
-        //The function returns whe weather information for a specific city from "weatherapi"
-        static async Task<Weather> GetWeatherAsync(string url)
+        // The function returns whe weather information for a specific city from "weatherapi"
+        private static async Task<Weather> GetWeatherAsync(string url)
         {
             var response = await client.GetAsync(baseApiWeather + url);
             var responseString = await response.Content.ReadAsStringAsync();
-            var weather = JsonConvert.DeserializeObject<Weather> (responseString);
-
+            var weather = JsonConvert.DeserializeObject<Weather>(responseString);
             return weather;            
-
         }
 
-        //The function contains the application logic
-        static async Task RunAsync()
+        // The function contains the application logic
+        private static async Task RunAsync()
         {                                   
             try
             {
@@ -97,30 +101,28 @@ namespace cityWeather
                 string resultString;
                 string days;
 
-                //Getting the cities
+                // Getting the cities
                 cities = await GetCityAsync();
                
-                //For each returned city, a weather information request made to "weatherapi"
+                // For each returned city, a weather information request made to "weatherapi"
                 foreach (var city in cities)
                 {
                     resultString = "Processed city {0} | ";
-                    weather = await GetWeatherAsync("forecast.json?key="+ weatherApiKey + "&q="+ city.latitude + ","+ city.longitude + "&days="+ weatherDays);
-                    forecastdays = weather.forecast.forecastday;
+                    weather = await GetWeatherAsync("forecast.json?key="+ weatherApiKey + "&q="+ city.Latitude + ","+ city.Longitude + "&days="+ weatherDays);
+                    forecastdays = weather.Forecast.Forecastday;
 
-                    //Here I could have explicitly get todays value with forecastday[0].day.condition.text and tomorrow's value with forecastday[1].day.condition.text, but I decided to use more generic approach.
+                    // Here I could have explicitly get todays value with forecastday[0].day.condition.text and tomorrow's value with forecastday[1].day.condition.text, but I decided to use more generic approach.
                     foreach (var forecastday in forecastdays)
                     {
-                        days = "";
-                        days += forecastday.day.condition.text + " - ";
-                        resultString = resultString + days;
-                        
+                        days = string.Empty;
+                        days += forecastday.Day.Condition.Text + " - ";
+                        resultString = resultString + days;                        
                     }
+
                     resultString = resultString.TrimEnd(' ');
                     resultString = resultString.TrimEnd('-');
-
-                    Console.WriteLine(resultString, weather.location.name);
+                    Console.WriteLine(resultString, weather.Location.Name);
                 }                               
-
             }
             catch (Exception e)
             {
